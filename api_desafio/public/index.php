5388fd5877ef5a3dc8f68bc01a01ff427f06ca8d
Carlos Eduardo <anjoazrael10@gmail.com>
2025-06-05 01:03:21 -0300

    feat(upload):Funcao de upload criada

--- api_desafio/public/index.php
+++ api_desafio/public/index.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
-
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
@@ -16,5 +15,6 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
 
 $app->handleRequest(Request::capture());
